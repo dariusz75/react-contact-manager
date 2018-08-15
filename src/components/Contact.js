d74958e4ed7e33fbd08408e2c896a1b3b5040c6b
Dariusz Franczak <darius32@op.pl>
2018-08-15 22:34:59 +0100

    done

---
+++ src/components/Contact.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h4>Dariusz Franczak</h4>
+        <ul>
+          <li>Email: darius32@op.pl</li>
+          <li>Phone: 07707636696</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default Contact;
